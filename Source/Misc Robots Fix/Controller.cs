d87f5b85a06f74a894edbc47f080815f8d7d7a1b
Tamas.Szekeres <firefoxpdm@yahoo.co.uk>
2020-03-22 15:43:20 +0100

    Do not disable robots' mined stuff

---
+++ Source/Misc Robots Fix/Controller.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace Misc_Robots_Fix
+{
+    public class Controller : Mod
+    {
+        public Controller(ModContentPack content) : base(content)
+        {
+            Harmony harmony = new Harmony("com.firefox.miscrobotsfix");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Source/Misc Robots Fix/PlantRestOverride.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace Misc_Robots_Fix
+{
+    class MineablePatch
+    {
+
+        [HarmonyPatch(typeof(Mineable))]
+        [HarmonyPatch("TrySpawnYield")]
+        static class TrySpawnYield
+        {
+            private static Pawn tempPawn;
+
+            static TrySpawnYield()
+            {
+                tempPawn = new Pawn();
+                tempPawn.GetType().GetField("factionInt", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(tempPawn, Faction.OfPlayer);
+                tempPawn.def = humanlikeDef();
+            }
+
+            private static ThingDef humanlikeDef()
+            {
+                RaceProperties humanlikeRace = new RaceProperties();
+                humanlikeRace.intelligence = Intelligence.Humanlike;
+                ThingDef def = new ThingDef();
+                def.race = humanlikeRace;
+                return def;
+            }
+
+            public static bool Prefix(MethodBase __originalMethod, Mineable __instance, Map map, float yieldChance, bool moteOnWaste, Pawn pawn)
+            {
+                if (pawn != null)
+                {
+                    if (!pawn.IsColonist)
+                    {
+                        if (pawn.ThingID.StartsWith("RPP_Bot_"))
+                        {
+                            __originalMethod.Invoke(__instance, new object[] { map, yieldChance, moteOnWaste, tempPawn });
+                            return false;
+                        }                        
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
